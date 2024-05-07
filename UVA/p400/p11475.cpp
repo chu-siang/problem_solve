2336899e5ab88bba8e503975d4fc8749824bc315
johnson <k32720101@gmail.com>
2024-05-08 02:17:46 +0800

    upload

---
+++ UVA/p400/p11475.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+signed main(){
+    string s;
+    while(cin >> s){
+        cout << s;
+        int next[s.size()] = {},i = 1, j = 0;
+        string t = s;
+        reverse(t.begin(),t.end());
+        while(i < t.size()){
+            if(t[i] == t[j])next[i++] = ++j;
+            else if(!j)i++;
+            else j = next[j-1];
+        }
+        i = 0,j = 0;
+        while(i < s.size()){
+            if(s[i] == t[j])i++,j++;
+            else if(!j)i++;
+            else j = next[j-1];
+        }
+        cout << t.substr(j,t.size()-j) << '\n';
+    }
+}
