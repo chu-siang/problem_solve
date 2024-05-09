db9c9e6d2a70c55b8923b1b7ca2715fb46e52b58
johnson <k32720101@gmail.com>
2024-05-09 16:32:54 +0800

    update

--- UVA/p100/p10000/P10093.cpp
+++ UVA/p100/p10000/P10093.cpp
@@ -1,38 +1,31 @@
 #include<bits/stdc++.h>
-#define ll long long
+#define int long long
 using namespace std;
 
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0),cout.tie(0);
+signed main(){
     string s;
-    bool flag = false;
     while(getline(cin,s)){
-        ll mx = 1;
-        int l = s.length();
         int sum = 0;
-        for(int i = 0; i < l; i++){
-            if(s[i] <= '9' && s[i] >= '0'){
-                sum += s[i]-'0';
-                if(s[i] - '0' > mx) mx = s[i] - '0';
-            }else if(s[i] <= 'Z' && s[i] >= 'A'){
-                // Z =35
-                sum += (s[i] - 'A'+10);
-                if(s[i] - 'A'+10 > mx) mx = s[i] - 'A'+10;
-            }else if(s[i] <= 'z' && s[i] >= 'a'){
-                sum += (s[i]-'a'+36);
-                if(s[i] - 'a' + 36 > mx) mx = s[i] -'a'+36;
-            }else continue;
+        int mx = 1;//預設二進位 btw 有可能有負號or空白
+        for(int i = 0; i < s.size(); i++){
+            int tmp = 0;
+            if(s[i] >= '0' && s[i] <= '9'){
+                tmp = s[i]-'0';
+            }else if(s[i] >= 'A' && s[i] <= 'Z'){
+                tmp = s[i]-'A' + 10;
+            }else if(s[i] >= 'a' && s[i] <= 'z'){
+                tmp = s[i] -'a' +36;
+            }
+            sum+= tmp;
+            if(tmp > mx) mx = tmp;
         }
         int i = mx;
-        for( ; i < 62; i++){
-            if((sum % i) == 0){
-                cout << i + 1 << '\n';
+        for(; i < 62; i++){
+            if(sum % i == 0){
+                cout << i+1 << '\n';
                 break;
             }
         }
-        if(i == 62){
-            cout << "such number is impossible!\n";
-        }
+        if(i == 62)cout << "such number is impossible!\n";
     }
 }
