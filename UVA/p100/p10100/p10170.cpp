385f4c61b5fb15ba7e2b880e3cc7ba9d3149796d
johnson <k32720101@gmail.com>
2024-05-17 06:12:27 +0800

    update

--- UVA/p100/p10100/p10170.cpp
+++ UVA/p100/p10100/p10170.cpp
@@ -1,28 +1,14 @@
 #include<bits/stdc++.h>
-#define ll long long
-#define ull unsigned long long
+#define int long long
 using namespace std;
 
-const int MAXN = 1e8+5;
-ull arr[MAXN] = {0};
-
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0),cout.tie(0);
-    ull s,d;
-    while(cin >> s >> d){
-        if(s >= d){
-            cout << s << '\n';
-            continue; 
+signed main(){
+    int n,d;
+    while(cin >> n >> d){
+        int i = n;
+        for(; i < d; i+=n){
+            n++;
         }
-        
-        arr[0] = s;
-        for(ull i = 1; i <= 1e8+5; i++){
-            arr[i] = (arr[i-1] + i+s);
-            if(arr[i] >= d){
-                cout << i+s << '\n';
-                break;
-            }
-        }
+        cout << n << '\n';
     }
 }
