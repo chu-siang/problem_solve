53da7e875f9e2c7598107dccb69822f4cf5b62fb
johnson <k32720101@gmail.com>
2024-05-20 15:58:35 +0800

    update

--- UVA/p200/p256.cpp
+++ UVA/p200/p256.cpp
@@ -1,37 +1,27 @@
 #include<bits/stdc++.h>
-#define ll long long
+#define int long long
 using namespace std;
-
-int main(){
-    ll n;
-    while(cin >> n){
-        if(n == 2){
-            cout << "00\n";
-            cout << "01\n";
-            cout << "81\n";
+int a[5] = {0,10,100,1000,10000};
+vector<int>v[5];
+void solve(){
+    for(int i = 0 ;i < 10000; i++){
+        int ii = i*i;
+        for(int j = 1; j < 5; j++){
+            if(i < a[j]){
+                int x = (ii/a[j] + ii%a[j]);
+                if(x == i)v[j].push_back(ii);
+            }
         }
-        else if(n == 4){
-            cout << "0000\n";
-            cout << "0001\n";
-            cout << "2025\n";
-            cout << "3025\n";
-            cout << "9801\n";
-        }else if(n == 6){
-            cout << "000000\n";
-            cout << "000001\n";
-            cout << "088209\n";
-            cout << "494209\n";
-            cout << "998001\n";
-        }else if(n == 8){
-            cout << "00000000\n";
-            cout << "00000001\n";
-            cout << "04941729\n";
-            cout << "07441984\n";
-            cout << "24502500\n";
-            cout << "25502500\n";
-            cout << "52881984\n";
-            cout << "60481729\n";
-            cout << "99980001\n";
+    }
+}
+
+signed main(){
+    int n;
+    solve();
+    while(cin >> n){
+        n >>= 1;
+        for(int i = 0; i < v[n].size(); i++){
+            cout <<setw(2*n) <<setfill('0')<< v[n][i] << '\n';
         }
     }
 }
