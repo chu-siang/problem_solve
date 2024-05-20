24a6d472a84caf6b1c174cab0be912d8c5a65acd
johnson <k32720101@gmail.com>
2024-05-21 01:38:38 +0800

    upload

---
+++ UVA/p100/p188.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+int x[100];
+int k,n;
+int c;
+void f(){
+    for(int i = 0; i < k; i++){
+        for(int j = i+1; j < k; j++){
+            if((c/x[i]%k) == (c/x[j]%k)){
+                c = min((c/x[i]+1)*x[i],(c/x[j]+1)*x[j]);
+                f();
+                return ;
+            }
+        }
+    }
+}
+signed main(){
+    string s;
+    while(getline(cin,s)){
+        memset(x,0,sizeof(x));
+        k = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == ' '){
+                k++;
+                while(s[i+1] == ' ')i++;
+            }else{
+                x[k] = x[k]*32 + s[i]-'a'+1;
+            }
+        }
+        k++;
+        sort(x,x+k);
+        c = x[0];
+        f();
+        cout << s << '\n' << c << '\n' << '\n';
+    }
+}
