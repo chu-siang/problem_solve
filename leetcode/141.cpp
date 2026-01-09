044f3af67fe7501995c7f0dbfe7ef011687ae48c
chu-siang <k32720101@gmail.com>
2026-01-09 15:51:30 +0000

    daily 1/9++

---
+++ leetcode/141.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *first = head;
+        ListNode *second = head;
+        while(first!=nullptr && first->next != nullptr){
+            first = first->next->next;
+            second = second->next;
+            if(first == second){
+                return true;
+            }
+        }
+        return false;
+    }
+};
