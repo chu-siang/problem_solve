08c6987a625411449e0b9fc75e895224b0b6ac83
chu-siang <k32720101@gmail.com>
2026-01-11 13:01:12 +0000

    update

--- leetcode/5.cpp
+++ leetcode/5.cpp
@@ -4,7 +4,7 @@ public:
         int n = s.size();
         if (n <= 1) return s;
 
-        // 1. 預處理：插入 #
+        //preprocessing
         string t;
         t.reserve(2 * n + 1);
         for (int i = 0; i < n; ++i) {
@@ -19,34 +19,34 @@ public:
         int center = 0, right = 0;
         int bestLen = 0, bestCenter = 0;
 
-        // 2. Manacher 主迴圈
+        // 2. Manacher 
         for (int i = 0; i < m; ++i) {
             int mirror = 2 * center - i;
 
             if (i < right)
                 p[i] = min(right - i, p[mirror]);
 
-            // 嘗試擴展
+            // try expansion
             while (i - p[i] - 1 >= 0 &&
                    i + p[i] + 1 < m &&
                    t[i - p[i] - 1] == t[i + p[i] + 1]) {
                 ++p[i];
             }
 
-            // 更新右界
+            // update right bound
             if (i + p[i] > right) {
                 center = i;
                 right = i + p[i];
             }
 
-            // 更新答案
+            // update answer
             if (p[i] > bestLen) {
                 bestLen = p[i];
                 bestCenter = i;
             }
         }
 
-        // 3. 還原回原字串 index
+        // return origin string index
         int start = (bestCenter - bestLen) / 2;
         return s.substr(start, bestLen);
     }
