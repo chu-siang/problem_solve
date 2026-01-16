c6b312faed833472318587ce8793e24a85173800
chu-siang <k32720101@gmail.com>
2026-01-16 16:06:21 +0000

    update

--- leetcode/8.cpp
+++ leetcode/8.cpp
@@ -1,15 +1,22 @@
-int atoi(const char *str) {
-    int sign = 1, base = 0, i = 0;
-    while (str[i] == ' ') { i++; }
-    if (str[i] == '-' || str[i] == '+') {
-        sign = 1 - 2 * (str[i++] == '-'); 
-    }
-    while (str[i] >= '0' && str[i] <= '9') {
-        if (base >  INT_MAX / 10 || (base == INT_MAX / 10 && str[i] - '0' > 7)) {
-            if (sign == 1) return INT_MAX;
-            else return INT_MIN;
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i = 0,base = 0;
+        int flag = 1; 
+        while(s[i] == ' ')i++;
+        if(s[i] == '+' || s[i] == '-'){
+            if(s[i] == '-')flag = -1;
+            i++;
         }
-        base  = 10 * base + (str[i++] - '0');
+        while(s[i] >= '0' && s[i] <= '9'){
+            if(base > INT_MAX/10 || (base == INT_MAX/10 && s[i] -'0' > 7)){
+                if(flag == 1)return INT_MAX;
+                return INT_MIN;
+            }
+            base *= 10;
+            base += (s[i] - '0');
+            i++;
+        }
+        return base*flag;
     }
-    return base * sign;
-}
+};
