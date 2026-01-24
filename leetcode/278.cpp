b748604d2cb1bd167ad21f5fac9bb7bc6dfa3585
chu-siang <k32720101@gmail.com>
2026-01-24 04:33:42 +0000

    daily 1/24++

---
+++ leetcode/278.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        // binary search 
+        int l = 1;
+        int r = n;
+        while (l < r) {
+            int m = l + ((r - l) / 2);
+            if (isBadVersion(m)) {
+                r = m;
+            }
+            else {
+                l = m + 1;
+            }
+        }
+        return l;
+    }
+};
