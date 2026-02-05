1e0a78891c895f12970c06aabf95b25673167d05
chu-siang <k32720101@gmail.com>
2026-02-05 14:40:49 +0000

    update daily 2/5

--- leetcode/208.cpp
+++ leetcode/208.cpp
@@ -1,3 +1,7 @@
+struct Node{
+    Node *char[26]{};
+    bool end = false;
+}
 class Trie {
 public:
     Trie() {
@@ -5,7 +9,9 @@ public:
     }
     
     void insert(string word) {
-        
+        for(int i = 0; i < n; ++ i){
+            
+        }
     }
     
     bool search(string word) {
@@ -24,3 +30,60 @@ public:
  * bool param_2 = obj->search(word);
  * bool param_3 = obj->startsWith(prefix);
  */
+
+ struct Node {
+    Node* son[26]{};
+    bool end = false;
+};
+
+class Trie {
+    Node* root = new Node();
+
+    int find(string word) {
+        Node* cur = root;
+        for (char c : word) {
+            c -= 'a';
+            if (cur->son[c] == nullptr) { // 道不同，不相为谋
+                return 0;
+            }
+            cur = cur->son[c];
+        }
+        // 走过同样的路（2=完全匹配，1=前缀匹配）
+        return cur->end ? 2 : 1;
+    }
+
+    void destroy(Node* node) {
+        if (node == nullptr) {
+            return;
+        }
+        for (Node* son : node->son) {
+            destroy(son);
+        }
+        delete node;
+    }
+
+public:
+    ~Trie() {
+        destroy(root);
+    }
+
+    void insert(string word) {
+        Node* cur = root;
+        for (char c : word) {
+            c -= 'a';
+            if (cur->son[c] == nullptr) { // if no rode
+                cur->son[c] = new Node(); // new it
+            }
+            cur = cur->son[c];
+        }
+        cur->end = true;
+    }
+
+    bool search(string word) {
+        return find(word) == 2;
+    }
+
+    bool startsWith(string prefix) {
+        return find(prefix) != 0;
+    }
+};
