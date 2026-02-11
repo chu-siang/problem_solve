511ff395ab71ae8b119f3880c8863234e93c10e4
chu-siang <k32720101@gmail.com>
2026-02-11 13:26:49 +0000

    daily 2/11

--- leetcode/208.cpp
+++ leetcode/208.cpp
@@ -1,89 +1,46 @@
 struct Node{
-    Node *char[26]{};
-    bool end = false;
-}
-class Trie {
-public:
-    Trie() {
-        
-    }
-    
-    void insert(string word) {
-        for(int i = 0; i < n; ++ i){
-            
-        }
-    }
-    
-    bool search(string word) {
-        
-    }
-    
-    bool startsWith(string prefix) {
-        
-    }
-};
-
-/**
- * Your Trie object will be instantiated and called as such:
- * Trie* obj = new Trie();
- * obj->insert(word);
- * bool param_2 = obj->search(word);
- * bool param_3 = obj->startsWith(prefix);
- */
-
- struct Node {
-    Node* son[26]{};
+    Node *son[26]{};
     bool end = false;
 };
-
-class Trie {
-    Node* root = new Node();
-
-    int find(string word) {
-        Node* cur = root;
-        for (char c : word) {
+class Trie{
+    Node *root = new Node();
+    int find(string word){
+        Node *cur = root;
+        for(char c : word){
             c -= 'a';
-            if (cur->son[c] == nullptr) { // 道不同，不相为谋
+            if(cur->son[c] == nullptr){
                 return 0;
             }
             cur = cur->son[c];
         }
-        // 走过同样的路（2=完全匹配，1=前缀匹配）
         return cur->end ? 2 : 1;
     }
-
-    void destroy(Node* node) {
-        if (node == nullptr) {
-            return;
-        }
-        for (Node* son : node->son) {
+    void destroy(Node *node){
+        if(node == nullptr)return ;
+        for(Node *son : node->son){
             destroy(son);
         }
         delete node;
     }
-
-public:
-    ~Trie() {
+public :
+    ~Trie(){
         destroy(root);
     }
-
-    void insert(string word) {
-        Node* cur = root;
-        for (char c : word) {
+    void insert(string word){
+        Node *cur = root;
+        for(char c : word){
             c -= 'a';
-            if (cur->son[c] == nullptr) { // if no rode
-                cur->son[c] = new Node(); // new it
+            if(cur->son[c] == nullptr){
+                cur->son[c] = new Node();
             }
             cur = cur->son[c];
         }
         cur->end = true;
     }
-
-    bool search(string word) {
+    bool search(string word){
         return find(word) == 2;
     }
-
-    bool startsWith(string prefix) {
-        return find(prefix) != 0;
+    bool startsWith(string prefix){
+        return find(prefix)!=0;
     }
 };
